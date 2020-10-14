af1ba3d2486c26e0715badaf4ca0122e1daf691b
smith-megan <smith.megan.design@gmail.com>
2020-10-14 10:39:30 -0700

    Updated CSS

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+// function getRndInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+// }
+
+
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    console.log(key)
 
     //no audio no worries
     if (!audio) { return }
 
     key.classList.add('playing')
+
+    //adding random placement of letters
+
+    //const topSpot = document.querySelector(`.playing`);
+    // topSpot.style.setProperty('--top-placement', getRndInteger(2, 9) * 10 + "%");
+
+
     audio.currentTime = .5
     audio.play();
+
+    function clearOutLetters() {
+        key.classList.remove('playing');
+        return
+    }
+    setTimeout(clearOutLetters, 7000);
+
 })
 
 const keys = document.querySelectorAll(`.key`)
@@ -19,4 +37,4 @@ function removeTransition(e) {
     }
 }
 
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
